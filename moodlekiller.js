9c201a0240e7704563ab64cad8e7557f33307116
zaurbbb <96561371+zaurbbb@users.noreply.github.com>
2022-03-15 08:20:02 +0600

    Update moodlekiller.js

--- moodlekiller.js
+++ moodlekiller.js
@@ -1,43 +1,43 @@
 //ONLY TOTAL GRADES
-$( '.a2:eq(0)' ).html("Foreign Language 2 (English) | Assel Salkenova");
+$( '.a2:eq(0)' ).html("Foreign Language 2 (English)");
 $( '.a2:eq(1)' ).html("85.00");
 
-$( '.a2:eq(2)' ).html("WEB Technologies 1 (Front End) | Yegemberdiyeva Gulzhan");
+$( '.a2:eq(2)' ).html("WEB Technologies 1 (Front End)");
 $( '.a2:eq(3)' ).html("78.00");
 
-$( '.a2:eq(4)' ).html("Object-Oriented Programming (Java) | Aimukhambetov Olzhas");
+$( '.a2:eq(4)' ).html("Object-Oriented Programming (Java)");
 $( '.a2:eq(5)' ).html("76.00");
 
-$( '.a2:eq(6)' ).html("Discrete Mathematics | Zhalgas Aidana");
+$( '.a2:eq(6)' ).html("Discrete Mathematics");
 $( '.a2:eq(7)' ).html("70.00");
 
-$( '.a2:eq(8)' ).html("Modern History of Kazakhstan | Mamytova Saule");
+$( '.a2:eq(8)' ).html("Modern History of Kazakhstan");
 $( '.a2:eq(9)' ).html("73.00");
 
-$( '.a2:eq(10)' ).html("Physical Culture 2 | Sarzhan Askapov");
+$( '.a2:eq(10)' ).html("Physical Culture 2");
 $( '.a2:eq(11)' ).html("70.00");
 
 
 
-$( '.a1:eq(0)' ).html("Foreign language 1 (English) | Aray Rakhimzhanova");
+$( '.a1:eq(0)' ).html("Foreign language 1 (English)");
 $( '.a1:eq(1)' ).html("85.00");
 
-$( '.a1:eq(2)' ).html("Information and Communication Technologies | Gulnur Smagulova");
+$( '.a1:eq(2)' ).html("Information and Communication Technologies");
 $( '.a1:eq(3)' ).html("78.00");
 
-$( '.a1:eq(4)' ).html("Introduction to Programming (C++) | Askar Khaimuldin\t");
+$( '.a1:eq(4)' ).html("Introduction to Programming (C++)");
 $( '.a1:eq(5)' ).html("76.00");
 
-$( '.a1:eq(6)' ).html("Linear Algebra | Balgaisha Mukanova");
+$( '.a1:eq(6)' ).html("Linear Algebra");
 $( '.a1:eq(7)' ).html("70.00");
 
-$( '.a1:eq(8)' ).html("Cultural studies | Anar Uyzbayeva\t");
+$( '.a1:eq(8)' ).html("Cultural studies");
 $( '.a1:eq(9)' ).html("73.00");
 
-$( '.a1:eq(10)' ).html("Physical Culture 1 | Sarzhan Askapov\t");
+$( '.a1:eq(10)' ).html("Physical Culture");
 $( '.a1:eq(11)' ).html("70.00");
 
-$( '.a1:eq(12)' ).html("Educational Practice | Gulnur Smagulova\t");
+$( '.a1:eq(12)' ).html("Educational Practice");
 $( '.a1:eq(13)' ).html("94.00");
 
 //FULL GRADES
@@ -50,42 +50,42 @@ $( '.aa1:eq()' ).html("");
 $( '.aa1:eq()' ).html("");
 $( '.aa1:eq()' ).html("");
 */
-$( '.aa3:eq(0)' ).html("Professional English | ");
+$( '.aa3:eq(0)' ).html("Professional English");
 $( '.aa3:eq(1)' ).html("0");
 $( '.aa3:eq(2)' ).html("0");
 $( '.aa3:eq(3)' ).html("0");
 $( '.aa3:eq(4)' ).html("0");
 $( '.aa3:eq(5)' ).html("0");
 
-$( '.aa3:eq(6)' ).html("WEB Technologies 2 (Back End) | ");
+$( '.aa3:eq(6)' ).html("WEB Technologies 2 (Back End)");
 $( '.aa3:eq(7)' ).html("");
 $( '.aa3:eq(8)' ).html("");
 $( '.aa3:eq(9)' ).html("");
 $( '.aa3:eq(10)' ).html("");
 $( '.aa3:eq(11)' ).html("");
 
-$( '.aa3:eq(12)' ).html("Algorithms and Data Structures | ");
+$( '.aa3:eq(12)' ).html("Algorithms and Data Structures");
 $( '.aa3:eq(13)' ).html("");
 $( '.aa3:eq(14)' ).html("");
 $( '.aa3:eq(15)' ).html("");
 $( '.aa3:eq(16)' ).html("");
 $( '.aa3:eq(17)' ).html("");
 
-$( '.aa3:eq(18)' ).html("Calculus 1 | ");
+$( '.aa3:eq(18)' ).html("Calculus 1");
 $( '.aa3:eq(19)' ).html("");
 $( '.aa3:eq(20)' ).html("");
 $( '.aa3:eq(21)' ).html("");
 $( '.aa3:eq(22)' ).html("");
 $( '.aa3:eq(23)' ).html("");
 
-$( '.aa3:eq(24)' ).html("Coding Lab | ");
+$( '.aa3:eq(24)' ).html("Coding Lab");
 $( '.aa3:eq(25)' ).html("");
 $( '.aa3:eq(26)' ).html("");
 $( '.aa3:eq(27)' ).html("");
 $( '.aa3:eq(28)' ).html("");
 $( '.aa3:eq(29)' ).html("");
 
-$( '.aa3:eq(30)' ).html("Physical Culture 3 | Sarzhan Askapov");
+$( '.aa3:eq(30)' ).html("Physical Culture 3");
 $( '.aa3:eq(31)' ).html("");
 $( '.aa3:eq(32)' ).html("");
 $( '.aa3:eq(33)' ).html("");
@@ -94,42 +94,42 @@ $( '.aa3:eq(35)' ).html("");
 $( '.aa3:eq(36)' ).html("");
 
 
-$( '.aa2:eq(0)' ).html("Foreign Language 2 (English) | Assel Salkenova");
+$( '.aa2:eq(0)' ).html("Foreign Language 2 (English");
 $( '.aa2:eq(1)' ).html("75.84");
 $( '.aa2:eq(2)' ).html("66.24");
 $( '.aa2:eq(3)' ).html("71.00");
 $( '.aa2:eq(4)' ).html("89.00");
 $( '.aa2:eq(5)' ).html("78.00");
 
-$( '.aa2:eq(6)' ).html("WEB Technologies 1 (Front End) | Yegemberdiyeva Gulzhan");
+$( '.aa2:eq(6)' ).html("WEB Technologies 1 (Front End)");
 $( '.aa2:eq(7)' ).html("96.00");
 $( '.aa2:eq(8)' ).html("97.33");
 $( '.aa2:eq(9)' ).html("97.00");
 $( '.aa2:eq(10)' ).html("100.00");
 $( '.aa2:eq(11)' ).html("98.00");
 
-$( '.aa2:eq(12)' ).html("Object-Oriented Programming (Java) | Aimukhambetov Olzhas");
+$( '.aa2:eq(12)' ).html("Object-Oriented Programming (Java)");
 $( '.aa2:eq(13)' ).html("82.00");
 $( '.aa2:eq(14)' ).html("79.89");
 $( '.aa2:eq(15)' ).html("81.00");
 $( '.aa2:eq(16)' ).html("63.33");
 $( '.aa2:eq(17)' ).html("74.00");
 
-$( '.aa2:eq(18)' ).html("Discrete Mathematics | Zhalgas Aidana");
+$( '.aa2:eq(18)' ).html("Discrete Mathematics");
 $( '.aa2:eq(19)' ).html("76.00");
 $( '.aa2:eq(20)' ).html("77.00");
 $( '.aa2:eq(21)' ).html("77.00");
 $( '.aa2:eq(22)' ).html("92.50");
 $( '.aa2:eq(23)' ).html("83.00");
 
-$( '.aa2:eq(24)' ).html("Modern History of Kazakhstan | Mamytova Saule\n");
+$( '.aa2:eq(24)' ).html("Modern History of Kazakhstan");
 $( '.aa2:eq(25)' ).html("76.00");
 $( '.aa2:eq(26)' ).html("88.00");
 $( '.aa2:eq(27)' ).html("82.00");
 $( '.aa2:eq(28)' ).html("85.00");
 $( '.aa2:eq(29)' ).html("83.00")
 
-$( '.aa2:eq(30)' ).html("Physical Culture 2 | Askapov Sarzhan");
+$( '.aa2:eq(30)' ).html("Physical Culture 2");
 $( '.aa2:eq(31)' ).html("80.00");
 $( '.aa2:eq(32)' ).html("60.00");
 $( '.aa2:eq(33)' ).html("70.00");
@@ -138,49 +138,49 @@ $( '.aa2:eq(35)' ).html("70.00");
 
 
 
-$( '.aa1:eq(0)' ).html("Foreign Language 1 (English) | Aray Rakhimzhanova");
+$( '.aa1:eq(0)' ).html("Foreign Language 1 (English));
 $( '.aa1:eq(1)' ).html("85.67");
 $( '.aa1:eq(2)' ).html("78.33");
 $( '.aa1:eq(3)' ).html("82.00");
 $( '.aa1:eq(4)' ).html("90.00");
 $( '.aa1:eq(5)' ).html("85.00");
 
-$( '.aa1:eq(6)' ).html("Information and Communication Technologies | Gulnur Smagulova");
+$( '.aa1:eq(6)' ).html("Information and Communication Technologies");
 $( '.aa1:eq(7)' ).html("68.90");
 $( '.aa1:eq(8)' ).html("87.40");
 $( '.aa1:eq(9)' ).html("78.00");
 $( '.aa1:eq(10)' ).html("78.00");
 $( '.aa1:eq(11)' ).html("78.00");
 
-$( '.aa1:eq(12)' ).html("Introduction to Programming (C++) | Askar Khaimuldin");
+$( '.aa1:eq(12)' ).html("Introduction to Programming (C++)");
 $( '.aa1:eq(13)' ).html("82.00");
 $( '.aa1:eq(14)' ).html("78.00");
 $( '.aa1:eq(15)' ).html("80.00");
 $( '.aa1:eq(16)' ).html("70.00");
 $( '.aa1:eq(17)' ).html("76.00");
 
-$( '.aa1:eq(18)' ).html("Linear Algebra | Balgaisha Mukanova\n");
+$( '.aa1:eq(18)' ).html("Linear Algebra");
 $( '.aa1:eq(19)' ).html("70.00");
 $( '.aa1:eq(20)' ).html("70.00");
 $( '.aa1:eq(21)' ).html("70.00");
 $( '.aa1:eq(22)' ).html("70.00");
 $( '.aa1:eq(23)' ).html("70.00");
 
-$( '.aa1:eq(24)' ).html("Cultural studies | Anar Uyzbayeva\n");
+$( '.aa1:eq(24)' ).html("Cultural studies");
 $( '.aa1:eq(25)' ).html("59.00");
 $( '.aa1:eq(26)' ).html("69.25");
 $( '.aa1:eq(27)' ).html("69.25");
 $( '.aa1:eq(28)' ).html("69.25");
 $( '.aa1:eq(29)' ).html("73.00")
 
-$( '.aa1:eq(30)' ).html("Physical Culture 1 | Sarzhan Askapov\n");
+$( '.aa1:eq(30)' ).html("Physical Culture 1");
 $( '.aa1:eq(31)' ).html("65.00");
 $( '.aa1:eq(32)' ).html("75.00");
 $( '.aa1:eq(33)' ).html("70.00");
 $( '.aa1:eq(34)' ).html("70.00");
 $( '.aa1:eq(35)' ).html("70.00");
 
-$( '.aa1:eq(36)' ).html("Educational Practice | Gulnur Smagulova");
+$( '.aa1:eq(36)' ).html("Educational Practice");
 $( '.aa1:eq(37)' ).html("");
 $( '.aa1:eq(38)' ).html("");
 $( '.aa1:eq(39)' ).html("");
