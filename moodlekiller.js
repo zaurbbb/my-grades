a7682bef430596bb4b2f3cae70343b1b55995d0e
zaurbbb <zaurberdibekov04@gmail.com>
2022-05-28 22:35:48 +0600

    upd

---
+++ moodlekiller.js
@@ -0,0 +1,528 @@
+//ONLY TOTAL GRADES
+
+//6th trim
+{
+    $( '.a6:eq(0)' ).html("Operating Systems");
+    $( '.a6:eq(1)' ).html("0.00");
+
+    $( '.a6:eq(2)' ).html("Computer Networks");
+    $( '.a6:eq(3)' ).html("0.00");
+
+    $( '.a6:eq(4)' ).html("Advanced programming 2");
+    $( '.a6:eq(5)' ).html("0.00");
+
+    $( '.a6:eq(6)' ).html("Probability and Statistics");
+    $( '.a6:eq(7)' ).html("0.00");
+
+    $( '.a6:eq(8)' ).html("Industrial Practice");
+    $( '.a6:eq(9)' ).html("0.00");
+
+    $( '.a6:eq(10)' ).html("Psychology");
+    $( '.a6:eq(11)' ).html("0.00");
+}
+
+//5th trim
+{
+$( '.a5:eq(0)' ).html("Russian Language 2");
+$( '.a5:eq(1)' ).html("0.00");
+
+$( '.a5:eq(2)' ).html("Advanced Databases (NoSQL)");
+$( '.a5:eq(3)' ).html("0.00");
+
+$( '.a5:eq(4)' ).html("Advanced programming 1");
+$( '.a5:eq(5)' ).html("0.00");
+
+$( '.a5:eq(6)' ).html("Computational Mathematics");
+$( '.a5:eq(7)' ).html("0.00");
+
+$( '.a5:eq(8)' ).html("Computer Organization and Architecture");
+$( '.a5:eq(9)' ).html("0.00");
+
+$( '.a5:eq(10)' ).html("Political Science");
+$( '.a5:eq(11)' ).html("0.00");
+}
+
+//4th trim
+{
+$( '.a4:eq(0)' ).html("Russian Language 1");
+$( '.a4:eq(1)' ).html("0.00");
+
+$( '.a4:eq(2)' ).html("Database Management Systems");
+$( '.a4:eq(3)' ).html("0.00");
+
+$( '.a4:eq(4)' ).html("Design and Analysis of Algorithms");
+$( '.a4:eq(5)' ).html("0.00");
+
+$( '.a4:eq(6)' ).html("Calculus 2");
+$( '.a4:eq(7)' ).html("0.00");
+
+$( '.a4:eq(8)' ).html("Software Design Patterns");
+$( '.a4:eq(9)' ).html("0.00");
+
+$( '.a4:eq(10)' ).html("Physical Culture 4");
+$( '.a4:eq(11)' ).html("0.00");
+}
+
+//3rd trim
+{
+$( '.a3:eq(0)' ).html("Professional English | Ainur Bakenova");
+$( '.a3:eq(1)' ).html("0.00");
+
+$( '.a3:eq(2)' ).html("WEB Technologies 2 (Back End) | Yelaman Apushev");
+$( '.a3:eq(3)' ).html("0.00");
+
+$( '.a3:eq(4)' ).html("Algorithms and Data Structures | Kyzyrkanov Abzal");
+$( '.a3:eq(5)' ).html("0.00");
+
+$( '.a3:eq(6)' ).html("Calculus 1 | Kassabek Dina");
+$( '.a3:eq(7)' ).html("0.00");
+
+$( '.a3:eq(8)' ).html("Coding Lab | Meruyert Nurgazy");
+$( '.a3:eq(9)' ).html("0.00");
+
+$( '.a3:eq(10)' ).html("Physical Culture 3 | Askapov Sarzhan");
+$( '.a3:eq(11)' ).html("0.00");
+}
+
+//2nd trim
+{
+$( '.a2:eq(0)' ).html("Foreign Language 2 (English) | Assel Salkenova");
+$( '.a2:eq(1)' ).html("78.00");
+
+$( '.a2:eq(2)' ).html("WEB Technologies 1 (Front End) | Yegemberdiyeva Gulzhan");
+$( '.a2:eq(3)' ).html("98.00");
+
+$( '.a2:eq(4)' ).html("Object-Oriented Programming (Java) | Aimukhambetov Olzhas");
+$( '.a2:eq(5)' ).html("74.00");
+
+$( '.a2:eq(6)' ).html("Discrete Mathematics | Zhalgas Aidana");
+$( '.a2:eq(7)' ).html("83.00");
+
+$( '.a2:eq(8)' ).html("Modern History of Kazakhstan | Mamytova Saule");
+$( '.a2:eq(9)' ).html("83.00");
+
+$( '.a2:eq(10)' ).html("Physical Culture 2 | Askapov Sarzhan");
+$( '.a2:eq(11)' ).html("70.00");
+}
+
+//1st trim
+{
+$( '.a1:eq(0)' ).html("Foreign language 1 (English) | Aray Rakhimzhanova");
+$( '.a1:eq(1)' ).html("85.00");
+
+$( '.a1:eq(2)' ).html("Information and Communication Technologies | Gulnur Smagulova");
+$( '.a1:eq(3)' ).html("78.00");
+
+$( '.a1:eq(4)' ).html("Introduction to Programming (C++) | Askar Khaimuldin");
+$( '.a1:eq(5)' ).html("76.00");
+
+$( '.a1:eq(6)' ).html("Linear Algebra | Balgaisha Mukanova");
+$( '.a1:eq(7)' ).html("70.00");
+
+$( '.a1:eq(8)' ).html("Cultural Studies | Anar Uyzbayeva");
+$( '.a1:eq(9)' ).html("73.00");
+
+$( '.a1:eq(10)' ).html("Physical Culture | Sarzhan Askapov");
+$( '.a1:eq(11)' ).html("70.00");
+
+$( '.a1:eq(12)' ).html("Educational Practice | Gulnur Smagulova");
+$( '.a1:eq(13)' ).html("94.00");
+}
+
+
+//FULL GRADES
+// number define trimester
+/*
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+*/
+
+//8th trim
+{
+    $('.aa8:eq(0)').html("");
+    $('.aa8:eq(1)').html("0");
+    $('.aa8:eq(2)').html("0");
+    $('.aa8:eq(3)').html("0");
+    $('.aa8:eq(4)').html("0");
+    $('.aa8:eq(5)').html("0");
+
+    $('.aa8:eq(6)').html("");
+    $('.aa8:eq(7)').html("0");
+    $('.aa8:eq(8)').html("0");
+    $('.aa8:eq(9)').html("0");
+    $('.aa8:eq(10)').html("0");
+    $('.aa8:eq(11)').html("0");
+
+    $('.aa8:eq(12)').html("");
+    $('.aa8:eq(13)').html("0");
+    $('.aa8:eq(14)').html("0");
+    $('.aa8:eq(15)').html("0");
+    $('.aa8:eq(16)').html("0");
+    $('.aa8:eq(17)').html("0");
+
+    $('.aa8:eq(18)').html("");
+    $('.aa8:eq(19)').html("0");
+    $('.aa8:eq(20)').html("0");
+    $('.aa8:eq(21)').html("0");
+    $('.aa8:eq(22)').html("0");
+    $('.aa8:eq(23)').html("0");
+
+    $('.aa8:eq(24)').html("");
+    $('.aa8:eq(25)').html("0");
+    $('.aa8:eq(26)').html("0");
+    $('.aa8:eq(27)').html("0");
+    $('.aa8:eq(28)').html("0");
+    $('.aa8:eq(29)').html("0");
+
+    $('.aa8:eq(30)').html("");
+    $('.aa8:eq(31)').html("0");
+    $('.aa8:eq(32)').html("0");
+    $('.aa8:eq(33)').html("0");
+    $('.aa8:eq(34)').html("0");
+    $('.aa8:eq(35)').html("0");
+}
+
+//7th trim
+{
+    $('.aa7:eq(0)').html("");
+    $('.aa7:eq(1)').html("0");
+    $('.aa7:eq(2)').html("0");
+    $('.aa7:eq(3)').html("0");
+    $('.aa7:eq(4)').html("0");
+    $('.aa7:eq(5)').html("0");
+
+    $('.aa7:eq(6)').html("");
+    $('.aa7:eq(7)').html("0");
+    $('.aa7:eq(8)').html("0");
+    $('.aa7:eq(9)').html("0");
+    $('.aa7:eq(10)').html("0");
+    $('.aa7:eq(11)').html("0");
+
+    $('.aa7:eq(12)').html("");
+    $('.aa7:eq(13)').html("0");
+    $('.aa7:eq(14)').html("0");
+    $('.aa7:eq(15)').html("0");
+    $('.aa7:eq(16)').html("0");
+    $('.aa7:eq(17)').html("0");
+
+    $('.aa7:eq(18)').html("");
+    $('.aa7:eq(19)').html("0");
+    $('.aa7:eq(20)').html("0");
+    $('.aa7:eq(21)').html("0");
+    $('.aa7:eq(22)').html("0");
+    $('.aa7:eq(23)').html("0");
+
+    $('.aa7:eq(24)').html("");
+    $('.aa7:eq(25)').html("0");
+    $('.aa7:eq(26)').html("0");
+    $('.aa7:eq(27)').html("0");
+    $('.aa7:eq(28)').html("0");
+    $('.aa7:eq(29)').html("0");
+
+    $('.aa7:eq(30)').html("");
+    $('.aa7:eq(31)').html("0");
+    $('.aa7:eq(32)').html("0");
+    $('.aa7:eq(33)').html("0");
+    $('.aa7:eq(34)').html("0");
+    $('.aa7:eq(35)').html("0");
+}
+
+//6th trim
+{
+    $('.aa6:eq(0)').html("");
+    $('.aa6:eq(1)').html("0");
+    $('.aa6:eq(2)').html("0");
+    $('.aa6:eq(3)').html("0");
+    $('.aa6:eq(4)').html("0");
+    $('.aa6:eq(5)').html("0");
+
+    $('.aa6:eq(6)').html("");
+    $('.aa6:eq(7)').html("0");
+    $('.aa6:eq(8)').html("0");
+    $('.aa6:eq(9)').html("0");
+    $('.aa6:eq(10)').html("0");
+    $('.aa6:eq(11)').html("0");
+
+    $('.aa6:eq(12)').html("");
+    $('.aa6:eq(13)').html("0");
+    $('.aa6:eq(14)').html("0");
+    $('.aa6:eq(15)').html("0");
+    $('.aa6:eq(16)').html("0");
+    $('.aa6:eq(17)').html("0");
+
+    $('.aa6:eq(18)').html("");
+    $('.aa6:eq(19)').html("0");
+    $('.aa6:eq(20)').html("0");
+    $('.aa6:eq(21)').html("0");
+    $('.aa6:eq(22)').html("0");
+    $('.aa6:eq(23)').html("0");
+
+    $('.aa6:eq(24)').html("");
+    $('.aa6:eq(25)').html("0");
+    $('.aa6:eq(26)').html("0");
+    $('.aa6:eq(27)').html("0");
+    $('.aa6:eq(28)').html("0");
+    $('.aa6:eq(29)').html("0");
+
+    $('.aa6:eq(30)').html("");
+    $('.aa6:eq(31)').html("0");
+    $('.aa6:eq(32)').html("0");
+    $('.aa6:eq(33)').html("0");
+    $('.aa6:eq(34)').html("0");
+    $('.aa6:eq(35)').html("0");
+}
+
+//5th trim
+{
+    $('.aa5:eq(0)').html("");
+    $('.aa5:eq(1)').html("0");
+    $('.aa5:eq(2)').html("0");
+    $('.aa5:eq(3)').html("0");
+    $('.aa5:eq(4)').html("0");
+    $('.aa5:eq(5)').html("0");
+
+    $('.aa5:eq(6)').html("");
+    $('.aa5:eq(7)').html("0");
+    $('.aa5:eq(8)').html("0");
+    $('.aa5:eq(9)').html("0");
+    $('.aa5:eq(10)').html("0");
+    $('.aa5:eq(11)').html("0");
+
+    $('.aa5:eq(12)').html("");
+    $('.aa5:eq(13)').html("0");
+    $('.aa5:eq(14)').html("0");
+    $('.aa5:eq(15)').html("0");
+    $('.aa5:eq(16)').html("0");
+    $('.aa5:eq(17)').html("0");
+
+    $('.aa5:eq(18)').html("");
+    $('.aa5:eq(19)').html("0");
+    $('.aa5:eq(20)').html("0");
+    $('.aa5:eq(21)').html("0");
+    $('.aa5:eq(22)').html("0");
+    $('.aa5:eq(23)').html("0");
+
+    $('.aa5:eq(24)').html("");
+    $('.aa5:eq(25)').html("0");
+    $('.aa5:eq(26)').html("0");
+    $('.aa5:eq(27)').html("0");
+    $('.aa5:eq(28)').html("0");
+    $('.aa5:eq(29)').html("0");
+
+    $('.aa5:eq(30)').html("");
+    $('.aa5:eq(31)').html("0");
+    $('.aa5:eq(32)').html("0");
+    $('.aa5:eq(33)').html("0");
+    $('.aa5:eq(34)').html("0");
+    $('.aa5:eq(35)').html("0");
+}
+
+//4th trim
+{
+    $('.aa4:eq(0)').html("Russian Language 1");
+    $('.aa4:eq(1)').html("0");
+    $('.aa4:eq(2)').html("0");
+    $('.aa4:eq(3)').html("0");
+    $('.aa4:eq(4)').html("0");
+    $('.aa4:eq(5)').html("0");
+
+    $('.aa4:eq(6)').html("Database Management Systems");
+    $('.aa4:eq(7)').html("0");
+    $('.aa4:eq(8)').html("0");
+    $('.aa4:eq(9)').html("0");
+    $('.aa4:eq(10)').html("0");
+    $('.aa4:eq(11)').html("0");
+
+    $('.aa4:eq(12)').html("Design and Analysis of Algorithms");
+    $('.aa4:eq(13)').html("0");
+    $('.aa4:eq(14)').html("0");
+    $('.aa4:eq(15)').html("0");
+    $('.aa4:eq(16)').html("0");
+    $('.aa4:eq(17)').html("0");
+
+    $('.aa4:eq(18)').html("Calculus 2");
+    $('.aa4:eq(19)').html("0");
+    $('.aa4:eq(20)').html("0");
+    $('.aa4:eq(21)').html("0");
+    $('.aa4:eq(22)').html("0");
+    $('.aa4:eq(23)').html("0");
+
+    $('.aa4:eq(24)').html("Software Design Patterns");
+    $('.aa4:eq(25)').html("0");
+    $('.aa4:eq(26)').html("0");
+    $('.aa4:eq(27)').html("0");
+    $('.aa4:eq(28)').html("0");
+    $('.aa4:eq(29)').html("0");
+
+    $('.aa4:eq(30)').html("Physical Culture 4");
+    $('.aa4:eq(31)').html("0");
+    $('.aa4:eq(32)').html("0");
+    $('.aa4:eq(33)').html("0");
+    $('.aa4:eq(34)').html("0");
+    $('.aa4:eq(35)').html("0");
+}
+
+//3rd trim
+{
+    $('.aa3:eq(0)').html("Professional English");
+    $('.aa3:eq(1)').html("78.77");
+    $('.aa3:eq(2)').html("81.21");
+    $('.aa3:eq(3)').html("80.00");
+    $('.aa3:eq(4)').html("0");
+    $('.aa3:eq(5)').html("0");
+
+    $('.aa3:eq(6)').html("WEB Technologies 2 (Back End)");
+    $('.aa3:eq(7)').html("96.00");
+    $('.aa3:eq(8)').html("95.00");
+    $('.aa3:eq(9)').html("96.00");
+    $('.aa3:eq(10)').html("0");
+    $('.aa3:eq(11)').html("0");
+
+    $('.aa3:eq(12)').html("Algorithms and Data Structures");
+    $('.aa3:eq(13)').html("100.00");
+    $('.aa3:eq(14)').html("100.00");
+    $('.aa3:eq(15)').html("100.00");
+    $('.aa3:eq(16)').html("0");
+    $('.aa3:eq(17)').html("0");
+
+    $('.aa3:eq(18)').html("Calculus 1");
+    $('.aa3:eq(19)').html("60.40");
+    $('.aa3:eq(20)').html("62.40");
+    $('.aa3:eq(21)').html("61.40");
+    $('.aa3:eq(22)').html("0");
+    $('.aa3:eq(23)').html("0");
+
+    $('.aa3:eq(24)').html("Coding Lab");
+    $('.aa3:eq(25)').html("93.00");
+    $('.aa3:eq(26)').html("90.00");
+    $('.aa3:eq(27)').html("92.00");
+    $('.aa3:eq(28)').html("0");
+    $('.aa3:eq(29)').html("0");
+
+    $('.aa3:eq(30)').html("Physical Culture 3");
+    $('.aa3:eq(31)').html("80.00");
+    $('.aa3:eq(32)').html("65.00");
+    $('.aa3:eq(33)').html("73.00");
+    $('.aa3:eq(34)').html("73.00");
+    $('.aa3:eq(35)').html("73.00");
+}
+//2nd trim
+{
+$( '.aa2:eq(0)' ).html("Foreign Language 2 (English)");
+$( '.aa2:eq(1)' ).html("75.84");
+$( '.aa2:eq(2)' ).html("66.24");
+$( '.aa2:eq(3)' ).html("71.00");
+$( '.aa2:eq(4)' ).html("89.00");
+$( '.aa2:eq(5)' ).html("78.00");
+
+$( '.aa2:eq(6)' ).html("WEB Technologies 1 (Front End)");
+$( '.aa2:eq(7)' ).html("96.00");
+$( '.aa2:eq(8)' ).html("97.33");
+$( '.aa2:eq(9)' ).html("97.00");
+$( '.aa2:eq(10)' ).html("100.00");
+$( '.aa2:eq(11)' ).html("98.00");
+
+$( '.aa2:eq(12)' ).html("Object-Oriented Programming (Java)");
+$( '.aa2:eq(13)' ).html("82.00");
+$( '.aa2:eq(14)' ).html("79.89");
+$( '.aa2:eq(15)' ).html("81.00");
+$( '.aa2:eq(16)' ).html("63.33");
+$( '.aa2:eq(17)' ).html("74.00");
+
+$( '.aa2:eq(18)' ).html("Discrete Mathematics");
+$( '.aa2:eq(19)' ).html("76.00");
+$( '.aa2:eq(20)' ).html("77.00");
+$( '.aa2:eq(21)' ).html("77.00");
+$( '.aa2:eq(22)' ).html("92.50");
+$( '.aa2:eq(23)' ).html("83.00");
+
+$( '.aa2:eq(24)' ).html("Modern History of Kazakhstan");
+$( '.aa2:eq(25)' ).html("76.00");
+$( '.aa2:eq(26)' ).html("88.00");
+$( '.aa2:eq(27)' ).html("82.00");
+$( '.aa2:eq(28)' ).html("85.00");
+$( '.aa2:eq(29)' ).html("83.00")
+
+$( '.aa2:eq(30)' ).html("Physical Culture 2");
+$( '.aa2:eq(31)' ).html("80.00");
+$( '.aa2:eq(32)' ).html("60.00");
+$( '.aa2:eq(33)' ).html("70.00");
+$( '.aa2:eq(34)' ).html("70.00");
+$( '.aa2:eq(35)' ).html("70.00");
+}
+
+//1st trim
+{
+    $('.aa1:eq(0)').html("Foreign Language 1 (English)");
+    $('.aa1:eq(1)').html("85.67");
+    $('.aa1:eq(2)').html("78.33");
+    $('.aa1:eq(3)').html("82.00");
+    $('.aa1:eq(4)').html("90.00");
+    $('.aa1:eq(5)').html("85.00");
+
+    $('.aa1:eq(6)').html("Information and Communication Technologies");
+    $('.aa1:eq(7)').html("68.90");
+    $('.aa1:eq(8)').html("87.40");
+    $('.aa1:eq(9)').html("78.00");
+    $('.aa1:eq(10)').html("78.00");
+    $('.aa1:eq(11)').html("78.00");
+
+    $('.aa1:eq(12)').html("Introduction to Programming (C++)");
+    $('.aa1:eq(13)').html("82.00");
+    $('.aa1:eq(14)').html("78.00");
+    $('.aa1:eq(15)').html("80.00");
+    $('.aa1:eq(16)').html("70.00");
+    $('.aa1:eq(17)').html("76.00");
+
+    $('.aa1:eq(18)').html("Linear Algebra");
+    $('.aa1:eq(19)').html("70.00");
+    $('.aa1:eq(20)').html("70.00");
+    $('.aa1:eq(21)').html("70.00");
+    $('.aa1:eq(22)').html("70.00");
+    $('.aa1:eq(23)').html("70.00");
+
+    $('.aa1:eq(24)').html("Cultural studies");
+    $('.aa1:eq(25)').html("59.00");
+    $('.aa1:eq(26)').html("69.25");
+    $('.aa1:eq(27)').html("64.00");
+    $('.aa1:eq(28)').html("84.50");
+    $('.aa1:eq(29)').html("73.00")
+
+    $('.aa1:eq(30)').html("Physical Culture 1");
+    $('.aa1:eq(31)').html("65.00");
+    $('.aa1:eq(32)').html("75.00");
+    $('.aa1:eq(33)').html("70.00");
+    $('.aa1:eq(34)').html("70.00");
+    $('.aa1:eq(35)').html("70.00");
+
+    $('.aa1:eq(36)').html("Educational Practice");
+    $('.aa1:eq(37)').html("");
+    $('.aa1:eq(38)').html("");
+    $('.aa1:eq(39)').html("");
+    $('.aa1:eq(40)').html("94.00");
+    $('.aa1:eq(41)').html("94.00");
+}
+
+/*
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+$( '.aa1:eq()' ).html("");
+*/
+
+//click method for selecting fieldset
+$(document).ready(function(){
+    $('fieldset').mouseover(function(){
+        $(this).css("border-color", "black");
+    });
+    $("fieldset").mouseout(function(){
+        $(this).css("border-color", "#dee2e6");
+    });
+});
+
