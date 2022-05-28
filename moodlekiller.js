866e7598f6aab25ccea867bc2d7f994043f3e427
zaurbbb <96561371+zaurbbb@users.noreply.github.com>
2022-05-28 19:32:51 +0600

    Update moodlekiller.js

--- moodlekiller.js
+++ moodlekiller.js
@@ -78,8 +78,8 @@ $( '.aa3:eq(5)' ).html("0");
 
 $( '.aa3:eq(6)' ).html("WEB Technologies 2 (Back End)");
 $( '.aa3:eq(7)' ).html("96.00");
-$( '.aa3:eq(8)' ).html("0");
-$( '.aa3:eq(9)' ).html("0");
+$( '.aa3:eq(8)' ).html("95.00");
+$( '.aa3:eq(9)' ).html("96.00");
 $( '.aa3:eq(10)' ).html("0");
 $( '.aa3:eq(11)' ).html("0");
 
